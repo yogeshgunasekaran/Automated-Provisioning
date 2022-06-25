437dcd6b7a283ae76fee823a360fd78652feaa4d
yogeshgunasekaran <106590073+yogeshgunasekaran@users.noreply.github.com>
2022-06-25 15:53:33 +0530

    Add files via upload

---
+++ nginx.sh
@@ -0,0 +1,32 @@
+# Installing nginx in Ubuntu	
+apt update
+apt install nginx -y
+cat <<EOT > backend
+upstream backend {
+
+	server app01:8080;
+
+}
+
+server {
+
+	listen 80;
+
+	location / {
+
+		proxy_pass http://backend;
+
+	}
+
+}
+
+EOT
+
+mv backend /etc/nginx/sites-available/backend
+rm -rf /etc/nginx/sites-enabled/default
+ln -s /etc/nginx/sites-available/backend /etc/nginx/sites-enabled/backend
+
+#starting nginx service and firewall
+systemctl start nginx
+systemctl enable nginx
+systemctl restart nginx
